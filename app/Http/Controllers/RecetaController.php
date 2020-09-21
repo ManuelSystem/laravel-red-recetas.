089c8846d77f8b1e10a5aa7f4d74b01e7297d09d
manuel <manuelsilva9908@gmail.com>
2020-09-21 18:49:13 -0500

    creando el resource RecetaController

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -7,21 +7,78 @@ use Illuminate\Http\Request;
 class RecetaController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke()
+    public function store(Request $request)
     {
-        $recetas = ['Receta pizza', 'Receta hamburguesa', 'Receta garbanzo'];
-        $categorias = ['Comida Mexicana', 'Comida Colombiana', 'Postres'];
-        //esta sintaxis es una forma de pasar datos a la vista desde aqui el controlador
-        /* return view('recetas.index')
-            ->with('recetas' . $recetas)
-            ->with('categorias', $categorias); */
+        //
+    }
 
-        //otra sintaxis para pasar datos a la vista es esta, que es mas compacta para usar
-        return view('recetas.index', compact('recetas', 'categorias'));
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
