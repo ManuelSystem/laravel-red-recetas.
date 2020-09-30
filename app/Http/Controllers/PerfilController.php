743a55ef277dbf537be8eac0f74baf2de9be5b06
manuel <manuelsilva9908@gmail.com>
2020-09-29 19:44:24 -0500

    se ha completado el update de perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Perfil;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -72,19 +73,45 @@ class PerfilController extends Controller
     public function update(Request $request, Perfil $perfil)
     {
         //validacion de datos
+        $data = request()->validate([
+            'nombre' => 'required',
+            'url' => 'required',
+            'biografia' => 'required'
+        ]);
 
+        //si el usuario sube una imagen
+        if ($request['imagen']) {
+            //obtener la ruta de la imagen
+            $ruta_imagen = $request['imagen']->store('upload-perfiles', 'public');
+
+            //Resize de la imagen, esto guarda la imagen con un ancho y altor predeterminado, se logra con la libreria de intervention image
+            $img = Image::make(public_path("storage/{$ruta_imagen}"))->fit(600, 600);
+            $img->save();
+
+            //crear un arreglo de imagen
+            $array_imagen = ['imagen' => $ruta_imagen];
+        }
+
+        //asignar nombre y url
+
+        auth()->user()->url = $data['url'];
+        auth()->user()->name = $data['nombre'];
+        auth()->user()->save();
+
+        //eliminar url y name de $data
+        unset($data['url']);
+        unset($data['nombre']);
 
         //Guardar información
-    }
+        //asignar biografia y imagen
+        $perfil->biografia = $data['biografia'];
+        auth()->user()->perfil()->update(array_merge( //array_merge toma 2 arreglos o más
+            $array_imagen ?? []
+        ));
+        $perfil->save();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Perfil  $perfil
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Perfil $perfil)
-    {
-        //
+
+        //redireccionar
+        return redirect()->action('RecetaController@index');
     }
 }
